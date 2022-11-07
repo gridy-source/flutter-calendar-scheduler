f022021100a9be9bd1b1e1e1677e7bfc1697c676
reality023 <reality023@naver.com>
2022-11-07 18:29:09 +0900

    initialized

---
+++ lib/component/calendar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:calendar_scheduler/main.dart';
+
+class Calendar extends StatelessWidget {
+  const Calendar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
