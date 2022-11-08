86e6d07c1426747f7163f49e8fe4e0d5e9e9a1cf
ncoresreality023 <reality023@n-cores.co.kr>
2022-11-09 01:01:48 +0900

    Code Generation 까지 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
+import 'package:calendar_scheduler/database/drift_database.dart';
 import 'package:calendar_scheduler/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:calendar_scheduler/database/drift_database.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  final database = LocalDatabase();
+
+  print(await database.getCategoryColors());
 
   runApp(
     MaterialApp(
